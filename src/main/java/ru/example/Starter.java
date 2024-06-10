7dcd8d870d393c9f4a56a7620779c6f5e5d7ea09
GitKIY <storage.GitHub.KIY@gmail.com>
2024-06-10 10:31:57 +0300

    first commit

---
+++ src/main/java/ru/example/Starter.java
@@ -0,0 +1,21 @@
+package ru.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Fraction fr= new Fraction(5,2);
+        Fractionable num =Utils.cache(fr);
+        System.out.println(num.doubleValue());// sout сработал
+        System.out.println(num.doubleValue());// sout молчит
+        System.out.println(num.doubleValue());// sout молчит
+        num.setNum(8);
+        System.out.println(num.doubleValue());// sout сработал
+        System.out.println(num.doubleValue());// sout молчит
+        System.out.println(num.doubleMult());
+        System.out.println(num.doubleMult());
+        num.setNum(10);
+        System.out.println(num.doubleValue());// sout сработал
+        System.out.println(num.doubleMult());
+        System.out.println(num.doubleValue());// sout молчит
+        System.out.println(num.doubleMult());
+    }
+}
---
+++ src/test/java/ru/example/TestFraction.java
@@ -0,0 +1,2 @@
+package ru.example;public class TestFraction {
+}
